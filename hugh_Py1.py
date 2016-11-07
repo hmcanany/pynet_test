5d4a9ac4b6452da1807b44263deda8d1da0e09d0
Hugh McAnany <hmcanan@its.jnj.com>
2016-11-07 09:10:02 -0800

    1st script created

---
+++ hugh_Py1.py
@@ -0,0 +1,6 @@
+n1 = 'George'
+n2 = 'Paul'
+n3 = 'John'
+print "The top 3 Beatles are {:>30}, {:>30}, and {:>30}".format(n1,n2,n3).format(n1,n2,n3)
+n4 = raw_input ("Who was the 4th Beatle? ")
+print n4 

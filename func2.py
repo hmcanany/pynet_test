58acdc5cf5171b21a297fa46e9a187cdc1e5bd1d
Hugh McAnany <hmcanan@its.jnj.com>
2016-11-08 12:01:22 -0500

    Updated func2.py

--- func2.py
+++ func2.py
@@ -7,7 +7,11 @@ c = 'from global'
 def fun(x, y, z):
     return x + y + z
 
-dict = {"one" : "Diction", "two" : "-ary", "three" : " Words"}
+d1 = {
+  'x' : 'Diction', 
+  'y' : '-ary', 
+  'z' : 'Words'
+}
 my_list = ['variables ', 'from ', 'list']
 #
 return_val = fun(*my_list)
@@ -16,5 +20,5 @@ print 'This is printing the function from my list {}.'.format(return_val)
 return_val = fun(a, b, c)
 print 'This is printing the function from my passed arguments {}.'.format(return_val)
 #
-return_val = fun(**dict)
+return_val = fun(**d1)
 print 'This is printing the function from my dictionary {}.'.format(return_val)

e01387314a32d43dd9c4349f62af5ac234e9841a
Hugh McAnany <hmcanan@its.jnj.com>
2016-11-09 09:35:24 -0500

    Directory Sync

---
+++ sh_ver1.py
@@ -0,0 +1,8 @@
+th open("show_version.txt") as f:
+    show_ver = f.read().splitlines()
+
+for line in show_ver:
+    if 'Processor board ID' in line:
+        _, serial_number = line.split("Processor board ID")
+
+print "\nSerial Number is {}\n".format(serial_number)
